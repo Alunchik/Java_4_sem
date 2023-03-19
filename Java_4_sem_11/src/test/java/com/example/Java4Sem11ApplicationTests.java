5dda17c6ebf2d48dbe4b6d03b4e695ee703b5b9c
Alunchik <alolachka@gmail.com>
2023-03-19 16:04:58 +0300

    11

---
+++ Java_4_sem_11/src/test/java/com/example/Java4Sem11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java4Sem11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
